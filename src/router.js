1d6599d5035fd8b93ac313cf8611e47f3900d965
aong13 <oaoa13@pukyong.ac.kr>
2025-01-16 17:54:41 +0900

    :sparkles: feat: 바텀탭 임시 구현

--- src/router.js
+++ src/router.js
@@ -4,20 +4,21 @@ import React from 'react';
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
-import MainPage from './pages/MainPage';
+import MainPage from './pages/MainPage/MainPage';
 import SearchPage from './pages/SearchPage';
 import UploadPage from './pages/UploadPage';
 import AlarmPage from './pages/AlarmPage';
 import Mypage from './pages/MyPage';
+import BottomTab from './components/BottomTab';
 // import SplashPage from './pages/SplashPage';
-// const renderBottomTabBar = props => <BottomTab {...props} />;
+const renderBottomTabBar = props => <BottomTab {...props} />;
 
 const MainTab = () => {
   return (
     <Tab.Navigator
+      tabBar={renderBottomTabBar}
       screenOptions={{
-        headerShown: false, // 헤더 비표시
-        tabBarShowLabel: true, // 텍스트만 표시
+        headerShown: false,
       }}>
       <Tab.Screen name="Home" component={MainPage} />
       <Tab.Screen name="Search" component={SearchPage} />
